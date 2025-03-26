4197d1c3f1f6a4ea6709f388fe1d74538343f478
princepanchal405 <princepanchal405@gmail.com>
2025-03-26 06:14:07 +0000

    datatype dtring

--- 01 basic.js/test.js
+++ 01 basic.js/test.js
@@ -1,1 +1,14 @@
-console.log("prince")
+const accountId = 121212
+let accountEmail = "prince@gmail.com"
+var accountPassword = "12345"
+accountCity = "rajastan"
+
+//accountId = 2
+
+accountEmail = "pc@pc.com"
+accountPassword = "99999"
+accountCity = "gujrat"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ datatype.js
@@ -0,0 +1,15 @@
+// //"use strict";
+
+// //alert(3+3) 
+
+// let Name = "PRINCE"
+// let Age = 19
+// let isLoggedIn = false
+// console.log(typeof "PRINC
+
+
+let someNumber = 33
+
+let stringNumber = String(stringNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
